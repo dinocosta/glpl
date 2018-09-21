0ede90995dd0987f97a8bad7ea2992274dc35bf2
joaofcosta <dinojoaocosta@gmail.com>
2018-09-21 23:43:42 +0100

    Create GLPL::Job class

---
+++ lib/glpl/job.rb
@@ -0,0 +1,15 @@
+class GLPL::Job
+
+  attr_reader :id, :status, :stage, :name, :ref, :created_at, :username
+
+  def initialize(job_data)
+    @id         = job_data["id"]
+    @status     = job_data["status"]
+    @stage      = job_data["stage"]
+    @name       = job_data["name"]
+    @ref        = job_data["ref"]
+    @created_at = job_data["created_at"]
+    @username   = job_data["username"]
+  end
+
+end
