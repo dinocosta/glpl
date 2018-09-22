7ecc2dd9c2da090313b6b92150957cece8ec6969
joaofcosta <dinojoaocosta@gmail.com>
2018-09-22 17:45:13 +0100

    Add tests for GLPL::Job

--- lib/glpl/job.rb
+++ lib/glpl/job.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 class GLPL::Job
 
   attr_reader :id, :status, :stage, :name, :ref, :created_at, :username
@@ -8,7 +10,7 @@ class GLPL::Job
     @stage      = job_data["stage"]
     @name       = job_data["name"]
     @ref        = job_data["ref"]
-    @created_at = job_data["created_at"]
+    @created_at = DateTime.parse(job_data["created_at"])
     @username   = job_data["username"]
   end
 
